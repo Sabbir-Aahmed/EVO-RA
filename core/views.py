0b2a05f083548bff9e4889c377c8af014b7fd2ac
Sabbir Ahmed <mdsabbir5820@gmail.com>
2025-07-10 20:56:25 +0600

    default home page

--- core/views.py
+++ core/views.py
@@ -1,3 +1,4 @@
 from django.shortcuts import render
 
-# Create your views here.
+def non_logged_home(request):
+    return render(request, 'non-logged-home.html')
--- user/urls.py
+++ user/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from user.views import sign_up, sign_in, sign_out , activate_user
+from core.views import non_logged_home
 urlpatterns = [
     path('sign-up/',sign_up, name="sign-up"),
     path('sign-in/',sign_in, name="sign-in"),
     path('sign-out/',sign_out, name="sign-out"),
     path('activate/<int:user_id>/<str:token>/', activate_user, name='activate-user'),
-    
+    path('evora/',non_logged_home, name='non_logged_home')
     
 ]
