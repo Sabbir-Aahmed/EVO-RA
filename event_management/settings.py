c72f0a7d90f12ed495d0e0ba68bb4c5530461ac7
Sabbir Ahmed <mdsabbir5820@gmail.com>
2025-07-12 17:37:40 +0600

    default database

--- event_management/settings.py
+++ event_management/settings.py
@@ -67,12 +67,12 @@ WSGI_APPLICATION = 'event_management.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 # DATABASES = {
 #     'default': {
@@ -85,13 +85,13 @@ WSGI_APPLICATION = 'event_management.wsgi.application'
 #     }
 # }
 
-DATABASES = {
-    'default': dj_database_url.config(
+# DATABASES = {
+#     'default': dj_database_url.config(
         
-        default=config('DATABASE_URL'),
-        conn_max_age=600
-    )
-}
+#         default=config('DATABASE_URL'),
+#         conn_max_age=600
+#     )
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
